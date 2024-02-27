061a2183c2ec7fb6f37803d768b9523cc0f2ef1a
magoreal4 <magoreal4@gmail.com>
2024-02-27 00:53:11 -0400

    first commit

--- config/settings.py
+++ config/settings.py
@@ -61,8 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
